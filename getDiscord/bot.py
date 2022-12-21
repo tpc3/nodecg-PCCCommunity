69e5d5ef2c2c74f9ab656c73fc27f9a6dbfaea08
CloverMidori <youhuid05@gmail.com>
2022-12-21 15:00:17 +0900

    Revert "不要になったgetDiscordの消去しませんごめんなさい...()"
    This reverts commit ca8390a33fb9c30d5a69de46bcf581e7ff6b2a9f.

---
+++ getDiscord/bot.py
@@ -0,0 +1,38 @@
+import discord
+import requests
+
+# This script requires development version of discord.py (2.0+)
+
+config = {
+    "token": "", # Discord token
+    "cid": 0, # Text channel ID
+    "vcid": 0, # Voice channel ID
+    "url": "http://localhost:9090/PCCCommunity-API/" # NodeCG API endpoint
+}
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Starting stat collection as {client.user}.')
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    members = []
+    for i in client.get_channel(config["vcid"]).members:
+        if not i.display_name.startswith("stream"):
+            members.append({"name": i.display_name, "avatar": str(i.display_avatar.url)})
+    requests.post(config["url"] + "vc", json={"members": members})
+
+@client.event
+async def on_message(message):
+    if message.author == client.user or message.content == "":
+        return
+    if message.channel.id == config["cid"]:
+        requests.post(config["url"] + "chat", json={"name": message.author.display_name, "avatar": str(message.author.avatar.url), "content": message.clean_content})
+
+client.run(config["token"])
